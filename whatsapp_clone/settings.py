cc5ef290909124643f7e172b81bbf3b5af13d4ce
denil kurian <denilkuriancc@gmail.com>
2023-05-31 09:38:09 +0530

    inits

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -100,7 +100,7 @@ CHANNEL_LAYERS = {
 }
 
 # CELERY SETTINGS
-CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_BROKER_URL = os.environ['REDIS_URL','redis://127.0.0.1:6379']
 #CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
